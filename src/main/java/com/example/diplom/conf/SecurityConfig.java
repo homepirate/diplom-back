3adb5cb1ef707e8961ef5159abbfb1d594437ad3
d50776310@gmail.com <d50776310@gmail.com>
2025-01-15 17:46:47 +0300

    add get all specializations

--- src/main/java/com/example/diplom/conf/SecurityConfig.java
+++ src/main/java/com/example/diplom/conf/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**", "/register/**").permitAll()
+                        .requestMatchers("/auth/**", "/register/**","/specializations").permitAll()
                         .requestMatchers("/doctor/**").hasRole("DOCTOR")
                         .requestMatchers("/patient/**").hasRole("PATIENT")
                         .anyRequest().authenticated()
